4214622872f042917d3bd8fe8f07b41269b792d6
Taris Henrique <taris.tj@hotmail.com>
2022-09-29 19:43:39 -0300

    Segundo Commit

---
+++ src/testeimovel/Imovel.java
@@ -0,0 +1,9 @@
+package testeimovel;
+
+public class Imovel {
+    protected String endereco;
+    protected double preco;
+
+    
+    
+}
---
+++ src/testeimovel/TesteImovel.java
@@ -0,0 +1,10 @@
+
+package testeimovel;
+
+public class TesteImovel {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
